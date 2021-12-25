fcf424ab112052e471caeb77a2e663e19cbdce59
lebedev_yua <lebedev_yua@proit.ru>
2021-12-25 09:39:42 +0300

    Fix time formating

--- src/helpers/timeFormater.ts
+++ src/helpers/timeFormater.ts
@@ -1,4 +1,19 @@
-const formatTime = (s: number) =>
-    (s - (s %= 60)) / 60 + (9 < s ? ':' : ':0') + s;
+function formatTime(duration: number) {
+    // Hours, minutes and seconds
+    var hrs = ~~(duration / 3600);
+    var mins = ~~((duration % 3600) / 60);
+    var secs = ~~duration % 60;
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = '';
+
+    if (hrs > 0) {
+        ret += '' + hrs + ':' + (mins < 10 ? '0' : '');
+    }
+
+    ret += '' + mins + ':' + (secs < 10 ? '0' : '');
+    ret += '' + secs;
+    return ret;
+}
 
 export default formatTime;
